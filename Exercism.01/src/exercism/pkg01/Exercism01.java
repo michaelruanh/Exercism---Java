976cf6a68d02f0ab2795c39b291011dd6437ce09
Michael Ruanh de Lima Ferreira <77198833+michaelruanh@users.noreply.github.com>
2022-02-21 19:01:37 -0300

    Add files via upload

---
+++ Exercism.01/src/exercism/pkg01/Exercism01.java
@@ -0,0 +1,14 @@
+
+package exercism.pkg01;
+
+
+public class Exercism01 {
+
+    
+    public static void main(String[] args) {
+            System.out.println("Hello, World!");
+    }
+
+    
+    }
+    
